c9a43319b8ab4f54193ead9bbd8287bb4cc115a3
Saket Karn <saketkarn38@gmail.com>
2024-04-03 00:27:00 +0530

    homepage

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero"
+const App = () => {
+  let heroData=[
+    {text1:"Dive into", text2:"what you love"},
+    {text1:"Indulge", text2:"your passions"},
+    {text1:"Give in to",text2:"your passions"},
+  ]
+
+  const [heroCount, setHeroCount]= useState(0);
+
+  const [playStatus, setPlayStatus]= useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeroCount((count) => {
+        const nextCount = count === 2 ? 0 : count + 1;
+        return nextCount;
+      });
+    }, 3000);
+  
+    return () => clearInterval(interval);
+  }, []);
+  
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
+      <Hero 
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </div>
+  )
+}
+
+export default App
